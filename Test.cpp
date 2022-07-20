98efc423cf597b7dbc4aa5b9752a2ccded9a3b1b
satore55 <109669885+satore55@users.noreply.github.com>
2022-07-20 14:23:44 +0200

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int number1,number2;
+
+int main()
+{
+ 
+   cin>>number1;
+   
+   ci>>number2;
+   
+   cout<<number1+number2;
+   
+   return 0;
+   
+      
+}
